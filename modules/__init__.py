78492e4bec0e5328f0d2d16c543bd00b065738d0
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-09 00:13:00 +0600

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -60,6 +60,7 @@ from .stikxutils.quote import setup_q_handler
 from .timexutils.times import setup_time_handler
 from .toolxutils.news import setup_news_handler
 from .toolxutils.rs import setup_rs_handler
+from .getusr import setup_getusr_handler
 from .toolxutils.vnote import setup_vnote_handler
 from .txtxutils.sptxt import setup_txt_handler
 from .webxutils.ws import setup_ws_handler
@@ -135,3 +136,4 @@ def setup_modules_handlers(app):
     setup_ss_handler(app)
     setup_ytag_handlers(app)
     setup_yth_handler(app)
+    setup_getusr_handler(app)
