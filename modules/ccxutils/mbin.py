899054bcb1f089fbb4a689dac790898b0645358b
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 21:17:10 +0600

    Update mbin.py

--- modules/ccxutils/mbin.py
+++ modules/ccxutils/mbin.py
@@ -35,7 +35,8 @@ def setup_mbin_handler(app: Client):
     async def bin_handler(client: Client, message: Message):
         # Check if user is banned
         user_id = message.from_user.id if message.from_user else None
-        if user_id and banned_users.find_one({"user_id": user_id}):
+        # FIX: Await the banned_users.find_one as it's an async call
+        if user_id and await banned_users.find_one({"user_id": user_id}):
             await client.send_message(message.chat.id, "**✘Sorry You're Banned From Using Me↯**")
             return
 
