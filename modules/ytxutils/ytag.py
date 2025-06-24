d46e80f1b4a5f287b91d8be11eb673968c05ca31
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 22:10:22 +0600

    Update ytag.py

--- modules/ytxutils/ytag.py
+++ modules/ytxutils/ytag.py
@@ -10,7 +10,8 @@ from core import banned_users           # Use banned_users
 
 async def ytag_handler(client: Client, message: Message):
     user_id = message.from_user.id if message.from_user else None
-    if user_id and banned_users.find_one({"user_id": user_id}):
+    # Await the banned_users check (Motor async)
+    if user_id and await banned_users.find_one({"user_id": user_id}):
         await client.send_message(
             message.chat.id,
             "**✘Sorry You're Banned From Using Me↯**",
