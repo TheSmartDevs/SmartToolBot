5ea7fe3f319733e59e17f668f103d3c2e7be44ed
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 20:42:14 +0600

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,5 +1,7 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
+# db.py
+# Copyright @ISmartDevs
+# Channel t.me/TheSmartDev
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import DB_URL
 from utils import LOGGER
@@ -8,7 +10,7 @@ LOGGER.info("Creating DB Client From DB_URL")
 
 try:
     channel_db_client = AsyncIOMotorClient(DB_URL)
-    channel_db = channel_db_client.get_database()
+    channel_db = channel_db_client.get_default_database()
     group_channel_bindings = channel_db["group_channel_bindings"]
     LOGGER.info("DB Client Successfully Created!")
 except Exception as e:
