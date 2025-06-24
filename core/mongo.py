f1ba9f58c9953655726a6b22a74ff265d3351d89
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 20:49:05 +0600

    Update mongo.py

--- core/mongo.py
+++ core/mongo.py
@@ -1,18 +1,23 @@
-# mongo.py
-# Copyright @ISmartDevs
-# Channel t.me/TheSmartDev
-
 from motor.motor_asyncio import AsyncIOMotorClient
+from urllib.parse import urlparse, parse_qs
 from utils import LOGGER
 from config import MONGO_URL
 
 LOGGER.info("Creating MONGO_CLIENT From MONGO_URL")
 
 try:
+    parsed = urlparse(MONGO_URL)
+    query_params = parse_qs(parsed.query)
+    db_name = query_params.get("appName", [None])[0]
+
+    if not db_name:
+        raise ValueError("No database name found in MONGO_URL (missing 'appName' query param)")
+
     MONGO_CLIENT = AsyncIOMotorClient(MONGO_URL)
-    db = MONGO_CLIENT.get_default_database()
+    db = MONGO_CLIENT.get_database(db_name)
     user_activity_collection = db["user_activity"]
-    LOGGER.info("MONGO_CLIENT Successfully Created!")
+
+    LOGGER.info(f"MONGO_CLIENT Successfully Created using database '{db_name}'!")
 except Exception as e:
     LOGGER.error(f"Failed to create MONGO_CLIENT: {e}")
     raise
