66aba96dbd96d1186a410df64fa317329671c6df
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 23:16:02 +0600

    Update info.py

--- modules/infoxutils/info.py
+++ modules/infoxutils/info.py
@@ -51,7 +51,7 @@ def setup_info_handler(app):
     async def handle_info_command(client: Client, message: Message):
         # Check if user is banned
         user_id = message.from_user.id if message.from_user else None
-        if user_id and banned_users.find_one({"user_id": user_id}):
+        if user_id and await banned_users.find_one({"user_id": user_id}):
             await client.send_message(message.chat.id, "**✘ Sorry You're Banned From Using Me ↯**")
             return
 
