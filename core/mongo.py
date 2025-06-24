54a0a101cae49f8408b3fa63bdca31f86ff9c9ca
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 20:41:41 +0600

    Update mongo.py

--- core/mongo.py
+++ core/mongo.py
@@ -1,16 +1,18 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
+# mongo.py
+# Copyright @ISmartDevs
+# Channel t.me/TheSmartDev
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from utils import LOGGER
 from config import MONGO_URL
 
 LOGGER.info("Creating MONGO_CLIENT From MONGO_URL")
+
 try:
     MONGO_CLIENT = AsyncIOMotorClient(MONGO_URL)
+    db = MONGO_CLIENT.get_default_database()
+    user_activity_collection = db["user_activity"]
     LOGGER.info("MONGO_CLIENT Successfully Created!")
 except Exception as e:
     LOGGER.error(f"Failed to create MONGO_CLIENT: {e}")
     raise
-
-db = MONGO_CLIENT.get_database()
-user_activity_collection = db["user_activity"]
