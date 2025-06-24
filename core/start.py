ad7d4ee2a2c72247a587e685f8e093e04802e427
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-06-24 23:07:09 +0600

    Update start.py

--- core/start.py
+++ core/start.py
@@ -5,11 +5,16 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.enums import ParseMode, ChatType
 import asyncio
 from config import UPDATE_CHANNEL_URL, COMMAND_PREFIX
-
+from core import banned_users
 
 def setup_start_handler(app: Client):
     @app.on_message(filters.command(["start"], prefixes=COMMAND_PREFIX) & (filters.private | filters.group))
     async def start_message(client: Client, message: Message):
+        user_id = message.from_user.id if message.from_user else None
+        if user_id and await banned_users.find_one({"user_id": user_id}):
+            await client.send_message(message.chat.id, "**✘ Sorry You're Banned From Using Me ↯**", parse_mode=ParseMode.MARKDOWN)
+            return
+
         chat_id = message.chat.id
 
         # Animation messages
@@ -31,9 +36,9 @@ def setup_start_handler(app: Client):
             response_text = (
                 f"<b>Hi {full_name}! Welcome To This Bot</b>\n"
                 "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit card tool, and more. Simplify your tasks with ease!\n"
+                f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit cards, and more. Simplify your tasks with ease!\n"
                 "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                "<b>Don't Forget To <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> For Updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
+                "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
             )
         elif message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
             # Default to group name if user is anonymous admin
@@ -49,18 +54,18 @@ def setup_start_handler(app: Client):
                 response_text = (
                     f"<b>Hi {full_name}! Welcome To This Bot</b>\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                    f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit card tool, and more. Simplify your tasks with ease!\n"
+                    f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit cards, and more. Simplify your tasks with ease!\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                    "<b>Don't Forget To <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> For Updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
+                    "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
                 )
             else:
                 # If user is an anonymous admin, use group name only
                 response_text = (
-                    f"<b>Hi! Welcome  {group_name} To This Bot</b>\n"
+                    f"<b>Hi! Welcome {group_name} To This Bot</b>\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                    f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit card tool, and more. Simplify your tasks with ease!\n"
+                    f"<b>Smart Tool ⚙️</b>: The ultimate toolkit on Telegram, offering education, AI, downloaders, temp mail, credit cards, and more. Simplify your tasks with ease!\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
-                    "<b>Don't Forget To <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> For Updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
+                    "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
                 )
 
         # Send message with inline buttons
