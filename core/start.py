e1db4fd389064008050d5ec057dad9e862bcc8c3
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-07-07 10:21:14 +0600

    form config import BAN_REPLY

--- core/start.py
+++ core/start.py
@@ -1,10 +1,10 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
+#Copyright @ISmartCoder
+#Updates Channel https://t.me/TheSmartDevs
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.enums import ParseMode, ChatType
 import asyncio
-from config import UPDATE_CHANNEL_URL, COMMAND_PREFIX
+from config import UPDATE_CHANNEL_URL, COMMAND_PREFIX, BAN_REPLY
 from core import banned_users
 
 def setup_start_handler(app: Client):
@@ -12,12 +12,11 @@ def setup_start_handler(app: Client):
     async def start_message(client: Client, message: Message):
         user_id = message.from_user.id if message.from_user else None
         if user_id and await banned_users.find_one({"user_id": user_id}):
-            await client.send_message(message.chat.id, "**✘ Sorry You're Banned From Using Me ↯**", parse_mode=ParseMode.MARKDOWN)
+            await client.send_message(message.chat.id, BAN_REPLY, parse_mode=ParseMode.MARKDOWN)
             return
 
         chat_id = message.chat.id
 
-        # Animation messages
         animation_message = await client.send_message(chat_id, "<b>Starting Smart Tool ⚙️...</b>", parse_mode=ParseMode.HTML)
         await asyncio.sleep(0.4)
         await client.edit_message_text(chat_id, animation_message.id, "<b>Generating Session Keys Please Wait...</b>", parse_mode=ParseMode.HTML)
@@ -25,14 +24,12 @@ def setup_start_handler(app: Client):
         await client.delete_messages(chat_id, animation_message.id)
 
         if message.chat.type == ChatType.PRIVATE:
-            # Extract full name in private chat
             full_name = "User"
             if message.from_user:
                 first_name = message.from_user.first_name or ""
                 last_name = message.from_user.last_name or ""
                 full_name = f"{first_name} {last_name}".strip()
 
-            # Private Chat Message
             response_text = (
                 f"<b>Hi {full_name}! Welcome To This Bot</b>\n"
                 "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
@@ -41,16 +38,13 @@ def setup_start_handler(app: Client):
                 "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
             )
         elif message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            # Default to group name if user is anonymous admin
             group_name = message.chat.title if message.chat.title else "this group"
 
-            # Check if user data is available (not anonymous admin)
             if message.from_user:
                 first_name = message.from_user.first_name or ""
                 last_name = message.from_user.last_name or ""
                 full_name = f"{first_name} {last_name}".strip()
 
-                # Personalized response for non-anonymous users
                 response_text = (
                     f"<b>Hi {full_name}! Welcome To This Bot</b>\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━</b>\n"
@@ -59,7 +53,6 @@ def setup_start_handler(app: Client):
                     "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
                 )
             else:
-                # If user is an anonymous admin, use group name only
                 response_text = (
                     f"<b>Hi! Welcome {group_name} To This Bot</b>\n"
                     "<b>━━━━━━━━━━━━━━━━━━━━━━━━━━</b>\n"
@@ -68,7 +61,6 @@ def setup_start_handler(app: Client):
                     "<b>Don't forget to <a href='{UPDATE_CHANNEL_URL}'>Join Here</a> for updates!</b>".format(UPDATE_CHANNEL_URL=UPDATE_CHANNEL_URL)
                 )
 
-        # Send message with inline buttons
         await client.send_message(
             chat_id=message.chat.id,
             text=response_text,
