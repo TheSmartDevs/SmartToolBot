7beceab3d83239ffa9681f1bec17c54cb202274e
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-02 16:37:16 +0600

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,3 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
 from .mongo import user_activity_collection
-from .database import auth_admins, banned_users
+from .database import auth_admins, banned_users, restart_messages
 from .start import setup_start_handler
