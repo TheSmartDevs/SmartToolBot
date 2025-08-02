aca10b7c338d2b867b9d126155d126b8dc8b4caa
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-02 16:36:25 +0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,4 +7,5 @@ from .dc_locations import get_dc_locations
 from .payment import handle_donate_callback, DONATION_OPTIONS_TEXT, get_donation_buttons, generate_invoice, timeof_fmt
 from .genbtn import responses, main_menu_keyboard, second_menu_keyboard, third_menu_keyboard
 from .pgbar import progress_bar
-from .nfy import notify_admin
+from .nfy import notify_admin, setup_nfy_handler
+
