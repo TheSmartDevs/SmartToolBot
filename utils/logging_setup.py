43e952a85694b0a9e1aaccce889c5496a57e0457
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-21 06:13:12 +0600

    Update logging_setup.py

--- utils/logging_setup.py
+++ utils/logging_setup.py
@@ -1,10 +1,8 @@
-#Copyright @ISmartDevs
-#Channel t.me/TheSmartDev
-#We Used A Just Logger Stream Handler Module To Capture Logs From Console Method By @abirxdhackz And @ISmartDevs
+#Copyright @ISmartCoder
+#Updates Channel t.me/TheSmartDev 
 import logging
 from logging.handlers import RotatingFileHandler
 
-# LOGGER SETUP TO CAPTURE LOGS SMOOTHLY THORUGH BOT USING /LOGS
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s", 
@@ -19,10 +17,10 @@ logging.basicConfig(
     ]
 )
 
-# Set Logging Levels For Specific Core Libraries  Pyrogram AIOHTTP APSCHEDULER TELETHON
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)  
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
+
 
 LOGGER = logging.getLogger(__name__)
