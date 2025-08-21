8a99db1ade34641a9fd020c6ecf4ba1b98032cab
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-08-21 06:12:21 +0600

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,10 +2,10 @@
 #Channel t.me/TheSmartDev
 #This Script Just For Importing Functions To Subscript Like A Package
 from .logging_setup import LOGGER
-from .getholiday import get_holidays   
 from .dc_locations import get_dc_locations
 from .payment import handle_donate_callback, DONATION_OPTIONS_TEXT, get_donation_buttons, generate_invoice, timeof_fmt
 from .genbtn import responses, main_menu_keyboard, second_menu_keyboard, third_menu_keyboard
 from .pgbar import progress_bar
 from .nfy import notify_admin, setup_nfy_handler
+
 
